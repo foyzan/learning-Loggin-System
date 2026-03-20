1d77fed54fa86e4b161330f9796b70d8804d2eb8
foyzan <foyzanahmedapon01@gmail.com>
2026-03-20 19:51:52 +0600

    File based logger setup for info and error logs

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -9,8 +9,22 @@ const consoleTransport = new transports.Console({
     format: combine(timestamp(), json(), errors({stack: true}))
 })
 
+// FileTransporter
+const fileTransporter = (level, filename) => {
+
+    return new transports.File({
+        level: level || "info",
+        format: combine(timestamp(), json()),
+        filename: filename || 'logs/info/info.log'
+    })
+}
+
+
+const infoFileTransporter = fileTransporter('info', 'logs/info/info.log')
+const errorFileTransporter = fileTransporter('error', 'logs/error/error.log')
+
 const logger = createLogger({
-    transports : [consoleTransport]
+    transports : [consoleTransport, infoFileTransporter, errorFileTransporter]
 })
 
 module.exports = logger
